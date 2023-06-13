89e8f87bb3f644a7a7310f83077bcbbf66d1e9a4
611727341 <611727341@BT-PF400F5T>
2023-06-13 15:04:43 +0100

    Add project files.

---
+++ Jazwood2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Jazwood2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Jazwood2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("081271FA-31C1-4A3C-B2C7-23B160D783AA")]
+[assembly: AssemblyVersion("1.0.0.0")]
